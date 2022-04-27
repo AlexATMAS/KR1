350de15ed82a0d87ba956fde407ae3ffc7752b58
ALex ATMAS <atmasbooking@gmail.com>
2022-04-27 15:44:58 +0300

    01

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    private String name;            // Имя сотрудника;
+    private String surName;         // Фамилия сотрудника;
+    private String midlName;        // Отчество сотрудника;
+    private float manyName;         // Зарплата сотрудника;
+    private int departmentName;     // Отдел сотрудника;
+    private static int id;          // id сотрудника;
+
+
+}
