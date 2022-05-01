5237087abe005b14b4cabe9eb1672880c20c41f0
ALex ATMAS <atmasbooking@gmail.com>
2022-05-02 00:13:09 +0300

    Повышенная сложность -
    1. Проиндексировать зарплату (вызвать изменение зарплат у всех сотрудников навеличину аргумента в %).
    2. Получить в качестве параметра номер отдела (1–5) и найти (всего 6 методов):
        а. Сотрудника с минимальной зарплатой.

--- src/StartApp.java
+++ src/StartApp.java
@@ -2,6 +2,8 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class StartApp {
+    private static  float percent = 15f; // индех ЗП процент;
+
 
 
     public static void main(String[] args) {
@@ -13,21 +15,30 @@ public class StartApp {
         employees[2] = new Employee(1,"Майорова","Арина","Тимофеевна", 80_500f);
         employees[3] = new Employee(2,"Медведева","Екатерина","Алексеевна", 25_000f);
         employees[4] = new Employee(2,"Никитин","Артур","Артёмович", 35_600f);
-        employees[5] = new Employee(2,"Морозов","Филипп","Анатольевич", 78_950f);
+        employees[5] = new Employee(2,"Морозов","Филипп","Анатольевич", 8_950f);
         employees[6] = new Employee(3,"Калинина","Ирина","Викторовна", 43_200f);
         employees[7] = new Employee(3,"Мартынов","Арсений","Петрович", 76_300f);
         employees[8] = new Employee(3,"Демидова","Сифия","Данииловна", 140_800.2555f);
         employees[9] = new Employee(4,"Михеев","Алесандр","Алексеевич", 35_900f);
 
-        getAllEmployee(employees);        /// a. Получить список всех сотрудников;
-        getMoneyAllEmployee(employees);   /// b. Сумма затрат на зарплаты в месяц;
-        getMinMoneyEmployee(employees);   /// c. Сотрудник с минимальной зарплатой;
-        getMaxMoneyEmployee(employees);   /// d. Сотрудник с максимальной зарплатой;
-        getAverageAllEmployee(employees); /// e. Среднее значение зарплат;
-        getAllNameEmployee(employees);      /// d. Получить Ф. И. О. всех сотрудников;
+        //getAllEmployee(employees);        /// a. Получить список всех сотрудников;
+        //getMoneyAllEmployee(employees);   /// b. Сумма затрат на зарплаты в месяц;
+        //getMinMoneyEmployee(employees);   /// c. Сотрудник с минимальной зарплатой;
+        //getMaxMoneyEmployee(employees);   /// d. Сотрудник с максимальной зарплатой;
+        //getAverageAllEmployee(employees); /// e. Среднее значение зарплат;
+        //getAllNameEmployee(employees);      /// d. Получить Ф. И. О. всех сотрудников;
+        //////////////////////////////////////////////////////////////////////////////////// Повышенная сложность
+        //getAllEmployee(employees); // список сотрудников ДО;
+        //setUpMoney(employees);  // Индех зарплат у всех сотрудников;
+        //getAllEmployee(employees); // список сотрудников ПОСЛЕ;
+
+        getDepMinMoneyEmployee(employees,4); // Сотрудник с минимальной зарплатой;
+
 
 
     }
+
+    //// Базовая сложность;
 
     private static void getAllEmployee(Employee[] employees) {
 
@@ -99,6 +110,31 @@ public class StartApp {
             System.out.println("\nID" + name);
         }
     } ////d. Получить Ф. И. О. всех сотрудников;
+
+    ////Повышенная сложность;
+    private static void setUpMoney(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            float upMoney = ((employees[i].getMoneyName() / 100f) * percent) + employees[i].getMoneyName();
+            employees[i].setMoneyName(upMoney);
+        }
+    } // Индех зарплат у всех сотрудников;
+
+    private static void getDepMinMoneyEmployee(Employee[] employees, int department) {
+        float min = employees[0].getMoneyName();
+        String name = "";
+        for (int i = 0; i < employees.length; i++) {
+            if (department == employees[i].getDepartmentName()) {
+                if (employees[i].getMoneyName() <= min) {
+                    min = employees[i].getMoneyName();
+                    name = employees[i].getSurName() + " " + employees[i].getName() + " " + employees[i].getMidlName();
+                }
+            }
+        }
+        System.out.println("Сотрудник с минимальной заработной платой: " + name
+                            + ".\nНаходиться в отделе: " + department
+                            + ".\nСумма заработной платы составляет: " + min);
+    }  // Сотрудник с минимальной зарплатой;
+
 
 
 
