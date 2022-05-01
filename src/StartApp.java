678c749a890f3e9ea522385bc46748a2e2335a23
ALex ATMAS <atmasbooking@gmail.com>
2022-05-01 22:25:28 +0300

    Базовая сложность - готово

--- src/StartApp.java
+++ src/StartApp.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 
 public class StartApp {
 
-    //static Employee employee;
 
     public static void main(String[] args) {
+
 
         Employee[] employees = new Employee[10];
         employees[0] = new Employee(1,"Силин","Алексей","Валерьевич", 1_000_000f);
@@ -16,13 +16,15 @@ public class StartApp {
         employees[5] = new Employee(2,"Морозов","Филипп","Анатольевич", 78_950f);
         employees[6] = new Employee(3,"Калинина","Ирина","Викторовна", 43_200f);
         employees[7] = new Employee(3,"Мартынов","Арсений","Петрович", 76_300f);
-        employees[8] = new Employee(3,"Демидова","Сифия","Данииловна", 140_800f);
+        employees[8] = new Employee(3,"Демидова","Сифия","Данииловна", 140_800.2555f);
         employees[9] = new Employee(4,"Михеев","Алесандр","Алексеевич", 35_900f);
 
-        //getAllEmployee(employees); //// a. Получить список всех сотрудников;
-        //getMoneyAllEmployee(employees); //// b. Сумма затрат на зарплаты в месяц;
-        //getMinMoneyEmployee(employees); /// c. Сотрудник с минимальной зарплатой;
-        //getMaxMoneyEmployee(employees); /// d. Сотрудник с максимальной зарплатой;
+        getAllEmployee(employees);        /// a. Получить список всех сотрудников;
+        getMoneyAllEmployee(employees);   /// b. Сумма затрат на зарплаты в месяц;
+        getMinMoneyEmployee(employees);   /// c. Сотрудник с минимальной зарплатой;
+        getMaxMoneyEmployee(employees);   /// d. Сотрудник с максимальной зарплатой;
+        getAverageAllEmployee(employees); /// e. Среднее значение зарплат;
+        getAllNameEmployee(employees);      /// d. Получить Ф. И. О. всех сотрудников;
 
 
     }
@@ -66,6 +68,39 @@ public class StartApp {
         }
         System.out.println("Сотрудник: " + name + " ,имеет максимальную заработную плату в размере : " + max + " рублей.");
     } /// d. Сотрудник с максимальной зарплатой;
+
+    /*private static void getAverageMoneyOneEmployee(Employee[] employees) {
+        float max = employees[0].getMoneyName();
+        float average = max / dayMonth;
+        String name = "";
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getMoneyName() >= max) {
+                max = employees[i].getMoneyName();
+                name = employees[i].getSurName() + " " + employees[i].getName() + " " + employees[i].getMidlName();
+            }
+        }
+        System.out.println("Средняя заработная плата сотрудника : " + name + ", составляет: " + average + " рублей.");
+
+
+    }*/ /// Среднее значение ЗП сотрудника с МАКСИМАЛЬНой ЗП;
+    private static void getAverageAllEmployee(Employee[] employees) {
+        float allMoney = 0f;
+        for (int i = 0; i < employees.length; i++) {
+
+            allMoney += employees[i].getMoneyName();
+        }
+        float ave = allMoney / employees.length;
+        System.out.println("Среднее значение выплат сотрудникам: "+ ave);
+    }  //// e. Среднее значение зарплат;
+
+    private static void getAllNameEmployee(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            String name = employees[i].getId() +" "+ employees[i].getSurName() +" "+ employees[i].getName() +" "+ employees[i].getMidlName();
+            System.out.println("\nID" + name);
+        }
+    } ////d. Получить Ф. И. О. всех сотрудников;
+
+
 
 
 
