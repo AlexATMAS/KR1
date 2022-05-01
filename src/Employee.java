083910bad8a0c311102e0f77b975893953faa770
ALex ATMAS <atmasbooking@gmail.com>
2022-05-01 17:33:20 +0300

    1. Создать класс Employee, который содержит информацию о Ф.И.О., отделе и зарплате сотрудника. Отделы для простоты должны быть названы от 1 до 5. 2. Добавить статическую переменную-счетчик, которая будет отвечать за id. 3. Добавить в класс Employee поле id, которое проставляется из счетчика, а затем счетчик увеличивает свое значение. 4. Добавить возможность получать значения полей из Employee (геттеры) для всех полей. 5. Добавить возможность устанавливать значения полей отдела и зарплаты (сеттеры). 6. По умолчанию все поля должны передавать через конструктор (кроме id) и заполняться в нем (включая id, который нужно получить из счетчика). 7. Создать внутри класса с методом main поле типа Employee[10], которое будет выполнять роль «хранилища» для записей о сотрудниках.

--- src/Employee.java
+++ src/Employee.java
@@ -2,9 +2,61 @@ public class Employee {
     private String name;            // Имя сотрудника;
     private String surName;         // Фамилия сотрудника;
     private String midlName;        // Отчество сотрудника;
-    private float manyName;         // Зарплата сотрудника;
+    private float moneyName;         // Зарплата сотрудника;
     private int departmentName;     // Отдел сотрудника;
-    private static int id;          // id сотрудника;
+    private int id = 0;             // id сотрудника;
+    private static int count;       // счетчик сотрудников;
 
 
+
+
+    public Employee(int departmentName,String surName, String name,String midlName,float moneyName) {
+        this.departmentName = departmentName;
+        this.surName = surName;
+        this.name = name;
+        this.midlName = midlName;
+        this.moneyName = moneyName;
+        count++;
+        this.id = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getMidlName() {
+        return midlName;
+    }
+
+    public float getMoneyName() {
+        return moneyName;
+    }
+
+    public int getDepartmentName() {
+        return departmentName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setMoneyName(float moneyName) {
+        this.moneyName = moneyName;
+    }
+
+    public void setDepartmentName(int departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + "ID-" + id + "\" " + "Информация о сотруднике: " + surName + " "  + name + " " + midlName + "."
+        +"Заработная плата сотрудника: " + moneyName + " рублей.";
+
+    }
 }
---
+++ src/StartApp.java
@@ -0,0 +1,22 @@
+public class StartApp {
+
+    public static void main(String[] args) {
+
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee(1,"Силин","Алексей","Валерьевич", 50_000);
+        employees[1] = new Employee(1,"Булгакова","Александра","Егоровна", 25_500);
+        employees[2] = new Employee(1,"Майорова","Арина","Тимофеевна", 80_500);
+        employees[3] = new Employee(2,"Медведева","Екатерина","Алексеевна", 25_000);
+        employees[4] = new Employee(2,"Никитин","Артур","Артёмович", 35_600);
+        employees[5] = new Employee(2,"Морозов","Филипп","Анатольевич", 78_950);
+        employees[6] = new Employee(3,"Калинина","Ирина","Викторовна", 43_200);
+        employees[7] = new Employee(3,"Мартынов","Арсений","Петрович", 15_300);
+        employees[8] = new Employee(3,"Демидова","Сифия","Данииловна", 140_800);
+        employees[9] = new Employee(4,"Михеев","Алесандр","Алексеевич", 98_900);
+
+    }
+
+
+
+
+}
